578bde2633296b558f5088f0e43abf3de70933d1
ramla02 <ramla2507@gmail.com>
2025-07-10 22:46:09 +0300

    multi languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+let firstLine = "C is fun";
+let secondLine = "Python is cool";
+let thirdLine = "JavaScript is amazing";
+
+console.log(firstLine); 
+console.log(secondLine);
+console.log(thirdLine);
