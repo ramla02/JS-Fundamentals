e604951850898ede7f93d171fe36bafe479eb66b
ramla02 <ramla2507@gmail.com>
2025-07-14 20:53:08 +0300

    cancat fixed

--- 4-concat.js
+++ 4-concat.js
@@ -3,7 +3,7 @@ const arg2 = process.argv[3];
 if (arg && arg2) {
 	console.log(arg + " is " + arg2);
 } else if (arg && !arg2) {
-	console.log(arg + " is  undefined");
+	console.log(arg + " is undefined");
 } else {
 	console.log("undefined is undefined");
 }
